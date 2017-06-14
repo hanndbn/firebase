53b6475ebb0c8046f00288b60b16630ec7f51c6b
hannd <hannd@oneempower.com>
2017-06-14 13:44:11 +0700

    update function

--- functions/index.js
+++ functions/index.js
@@ -8,50 +8,101 @@ admin.initializeApp(functions.config().firebase);
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
 exports.leaderboards = functions.https.onRequest((req, res) => {
-    // Grab the text parameter.
-    //const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-
-    let object = admin.database().ref().child('leaderboards');
+    let object = admin.database().ref('leaderboards');
     object.once('value', function (snapshot) {
         let leaderBoards = [];
         snapshot.forEach(function (childSnapshot) {
-                let highScore = childSnapshot.child('highScore').val();
-                let name = childSnapshot.child('name').val();
-                leaderBoards.push({userId: childSnapshot.key, highScore : highScore, name: name});
-        //     // });
-        //     let userScore = childSnapshot.parent;
-        //     let highScore = childSnapshot.val();
-        //     let name = userScore.child('name').val();
-        //     leaderBoards.push({userId: userScore.key, highScore: highScore, name: name});
+            let highScore = childSnapshot.child('highScore').val();
+            let userId = childSnapshot.child('userId').val();
+            leaderBoards.push({userId: userId, highScore: highScore});
         });
-        leaderBoards.sort(function (a,b) {
-           return parseInt(b.highScore) - parseInt(a.highScore)
+        leaderBoards.sort(function (a, b) {
+            return parseInt(b.highScore) - parseInt(a.highScore)
         });
         let rankBoards = [];
-        for(let i = 1; i <=leaderBoards.length; i++){
-            leaderBoards[i-1].rank = i;
+        for (let i = 1; i <= leaderBoards.length; i++) {
+            leaderBoards[i - 1].rank = i;
         }
         res.json(leaderBoards);
     });
-    //res.json(leaderBoards);
-    //res.render({leaderBoards:leaderBoards})
-    // admin.database().ref('/messages').push({original: original}).then(snapshot => {
-    //     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    //     res.redirect(303, snapshot.ref);
-    // });
+});
+exports.reportScore = functions.https.onRequest((req, res) => {
+    let requestUserId = req.body.userId;
+    let requestScore = req.body.score;
+    admin.database().ref('leaderboards').once('value', function (snapshot) {
+        let isExist = false;
+        let message = '';
+        snapshot.forEach(function (childSnapshot) {
+            let userId = childSnapshot.child('userId').val();
+            let highScore = childSnapshot.child('highScore').val();
+            if (requestUserId === userId) {
+                isExist = true;
+                if (parseInt(requestScore) > parseInt(highScore)) {
+                    childSnapshot.ref.update({
+                        highScore: requestScore
+                    })
+                    message = 'updated score success';
+                }else{
+                    message = 'score not need update';
+                }
+            }
+        });
+        if (!isExist) {
+            snapshot.ref.push({
+                userId: requestUserId,
+                highScore: requestScore
+            });
+            message = 'added new score to leaderboard';
+        }
+        return res.json({messsage: message});
+    });
 });
 
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-//     .onWrite(event => {
-//         // Grab the current value of what was written to the Realtime Database.
-//         const original = event.data.val();
-//         console.log('Uppercasing', event.params.pushId, original);
-//         const uppercase = original.toUpperCase();
-//         // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//         // writing to the Firebase Realtime Database.
-//         // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-//         return event.data.ref.parent.child('uppercase').set(uppercase);
-//     });
+exports.getTotalPlayers = functions.https.onRequest((req, res) => {
+    let userData = admin.database().ref('leaderboards');
+    userData.once('value', function (snapshot) {
+        let count = 0;
+        snapshot.forEach(function (childSnapshot) {
+            count++;
+        });
+        return res.json({totalPlayers: count});
+    });
+});
+
+exports.getLeaderboardPosition = functions.https.onRequest((req, res) => {
+    let requestUserId = req.body.userId;
+    let userData = admin.database().ref('leaderboards');
+    userData.once('value', function (snapshot) {
+        let rank = -1;
+        snapshot.forEach(function (childSnapshot) {
+            let userId = childSnapshot.child('userId').val();
+            if (requestUserId === userId) {
+                rank = childSnapshot.child('rank').val();
+            }
+        });
+        res.json({rank: rank});
+    });
+});
+
+exports.updateRankLeaderBoards = functions.database.ref('/leaderboards/{pushId}/highScore')
+    .onWrite(event => {
+        admin.database().ref('leaderboards').once('value', function (snapshot) {
+            let leaderBoards = [];
+            snapshot.forEach(function (childSnapshot) {
+                let highScore = childSnapshot.child('highScore').val();
+                let userId = childSnapshot.child('userId').val();
+                leaderBoards.push({userId: userId, highScore: highScore, childSnapshot : childSnapshot});
+            });
+            leaderBoards.sort(function (a, b) {
+                return parseInt(b.highScore) - parseInt(a.highScore)
+            });
+            for (let i = 1; i <= leaderBoards.length; i++) {
+                leaderBoards[i - 1].rank = i;
+            }
+
+            leaderBoards.forEach(function(item){
+                item.childSnapshot.ref.update({rank : item.rank});
+            });
+
+        });
+    });
